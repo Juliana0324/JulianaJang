e31b16d75208912243ae98dc4da53d8e6c2007ef
Juliana0324 <62527384+Juliana0324@users.noreply.github.com>
2021-08-29 17:52:26 +0900

    Update and rename String.js to StringArray.js

--- JavaScript/StringArray.js
+++ JavaScript/StringArray.js
@@ -40,3 +40,23 @@ let myString = 'Codeit';
 for (let str of myString) {
   console.log(str);
 }
+
+
+/*
+가장 중요한 차이는 배열은 'mutable(바뀔 수 있는)' 자료형인 반면 문자열은 'immutable(바뀔 수 없는)' 자료형이라는 것입니다.
+
+배열은 요소에 접근해서 할당연산자를 통해 요소를 수정할 수 있었죠?
+
+문자열은 한 번 할당된 값을 수정할 수 없습니다. 다르게 표현해서, 변수에 할당된 문자열을 바꾸고 싶다면, 일부를 바꾸는 게 아니라 새로운 문자열을 지정해주어야 한다는 것이죠.
+*/
+// 배열은 mutable
+let myArray = ['C', 'o', 'd', 'e', 'i', 't'];
+myArray[0] = 'B';
+console.log(myArray);
+
+// 문자열은 immutable
+let myString = 'Codeit';
+myString[0] = 'B';
+console.log(myString);
+(6) ["B", "o", "d", "e", "i", "t"]
+Codeit
