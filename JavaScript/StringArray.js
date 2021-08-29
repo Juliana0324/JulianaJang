5f1b3446646936e6624ac202e92407244d8f8b85
Juliana0324 <62527384+Juliana0324@users.noreply.github.com>
2021-08-29 18:10:11 +0900

    Update StringArray.js

--- JavaScript/StringArray.js
+++ JavaScript/StringArray.js
@@ -60,3 +60,28 @@ myString[0] = 'B';
 console.log(myString);
 (6) ["B", "o", "d", "e", "i", "t"]
 Codeit
+
+
+
+
+//예제
+//형돈이 가사만 출력
+
+let lyrics = "[재석]너에게 나 하고 싶었던 말 고마워 미안해 함께 있어서 할 수 있어서 웃을 수 있어[준하] 정말 고마웠어 내 손을 놓지 않아줘서 힘을 내볼게 함께 있다면 두렵지 않아[홍철] 내가 늘 웃으니까 내가 우습나 봐 하지만 웃을 거야 날 보고 웃는 너 좋아[명수] 자꾸만 도망치고 싶은데 저 화려한 큰 무대 위에 설 수 있을까? 자신 없어..[하하] 지금까지 걸어온 이 길을 의심하지는 마 잘못 든 길이 때론 지도를 만들었잖아[형돈] 혼자 걷는 이 길이 막막하겠지만 느리게 걷는 거야 천천히 도착해도 돼[길] 술 한 잔 하자는 친구의 말도 의미 없는 인사처럼 슬프게 들릴 때 날 찾아와";
+let memberName = '형돈';
+let memberPart = null;
+
+
+
+
+// 선택한 맴버의 시작파트 인덱스 값.
+let startSearch = lyrics.indexOf(`[${memberName}]`);
+// 선택한 맴버의 시작파트부터 문자열의 마지막 까지 슬라이스.
+memberPart = lyrics.slice(startSearch);
+// 잘려진 문자열 중 다음맴버의 시작파트 인덱스 값.
+let endSearch = memberPart.indexOf('[', 1);
+// endSearch 인덱스 값을 가지고 원하는 부분만 잘라냄.
+let test = memberPart.slice(0, endSearch);
+
+
+console.log(test);
